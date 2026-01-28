0820d4ed5fc9f861ef9f61b39bd043a0830222f1
Prakash Bohara <Prakash.Bohara@amdocs.com>
2026-01-28 12:48:47 +0530

    Updated Code

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-// app.js
 const http = require('http');
 const PORT = process.env.PORT || 3000;
-
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Kubernetes with Kustomize!\n');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        message: 'DevOps Tools List',
+        status: 'success',
+        devopsTools: [
+            'Jenkins',
+            'Docker',
+            'Kubernetes',
+            'Terraform',
+            'Ansible',
+            'GitLab CI/CD',
+            'GitHub Actions',
+            'CircleCI',
+            'Travis CI',
+            'Azure DevOps',
+            'AWS CodePipeline',
+            'Prometheus',
+            'Grafana',
+            'ELK Stack',
+            'Datadog',
+            'New Relic',
+            'Splunk',
+            'SonarQube',
+            'HashiCorp Vault',
+            'Helm',
+            'ArgoCD',
+            'Vagrant',
+            'Puppet',
+            'Chef',
+            'Salt'
+        ]
+    }));
 });
-
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
